5e8685e800ba8935be7005d690f16e73f56cb8ec
Kevin Pedro <kpedro88@gmail.com>
2025-11-17 16:10:11 -0600

    apply CHS refinement only if DeepFlavour is available

--- PhysicsTools/NanoAOD/python/nano_cff.py
+++ PhysicsTools/NanoAOD/python/nano_cff.py
@@ -312,6 +312,9 @@ def nanoAOD_customizeCommon(process):
     # Add lepton time-life info
     from PhysicsTools.NanoAOD.leptonTimeLifeInfo_common_cff import addTimeLifeInfoBase
     process = addTimeLifeInfoBase(process)
+    
+    process = nanoAOD_refineFastSim_puppiJet(process)
+    process = nanoAOD_refineFastSim_bTagDeepFlav(process, nanoAOD_addDeepInfoAK4CHS_switch.nanoAOD_addDeepFlavourTag_switch)
 
     return process
 
