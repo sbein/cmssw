a9730ce8d2f48b7bf285f83d1261d4a901cabae9
sbein <Samuel.bein@gmail.com>
2026-07-16 23:20:21 +0200

    tiny commit

--- FastSimulation/HighLevelTrigger/python/HLTSetup_cff.py
+++ FastSimulation/HighLevelTrigger/python/HLTSetup_cff.py
@@ -5,7 +5,7 @@ from FastSimulation.Tracking.hltElectronGsfTracks_cff import *
 from FastSimulation.Tracking.hltSeeds_cff import *
 from FastSimulation.Tracking.hltPixelTracks_cff import *
 
-# The hltbegin sequence (with L1 emulator)
+# hltbegin sequence (with L1 emulator)
 HLTBeginSequence = cms.Sequence(hltSeedSequence+hltPixelTracksFitter+hltPixelTracksFilter+hltPixelTracks)
 
 HLTBeginSequenceBPTX = cms.Sequence(HLTBeginSequence)
